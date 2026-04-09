3a5ca5835f1b777019127b8caaadec56e33cf321
DEV <DEV@NSN1191261171.edu119.local>
2026-04-09 09:43:28 -0300

    Guigui: Enviando projeto

---
+++ O_Super_Conversor_de_Temperaturas/Conversor de temperatura.cs
@@ -0,0 +1,21 @@
+﻿//Autoriza o uso de emoji e caracteres especiais
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+Console.WriteLine("Digite a Temperatura em C° 🌡️:");
+float.TryParse(Console.ReadLine(), out float celsius);
+float resultado = (celsius * 9 / 5) + 32;
+Console.WriteLine($"A temperatura é C°: {celsius}\nA conversão em F°: {resultado}");
+
+Console.WriteLine("Digite a tempetura em F°🤒:");
+float.TryParse(Console.ReadLine(), out float Fahrenheit);
+float resposta = (Fahrenheit - 32) * 5 / 9;
+Console.WriteLine($"A temperatura é F°: {Fahrenheit}\nA conversão em C°: {resposta}");
+
+Console.WriteLine("Digite a temperatura em K° 😰:");
+float.TryParse(Console.ReadLine(), out float Kelvin);
+float resto = (Kelvin - 292.15F);
+Console.WriteLine($"A temperatura é K°: {Kelvin}\nA conersão em C°: {resposta}");
+
+Console.WriteLine("Digite a temperatura em K°:");
+float.TryParse(Console.ReadLine(), out Kelvin);
+float F = (Kelvin - 292.15F) * 9 / 5 + 32;
+Console.WriteLine($"A temperatura é K°: {Kelvin}\nA convesão em F°: {resposta}");
